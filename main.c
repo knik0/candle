50a88e1cf637b3fabb9e3ae244324ad649933400
Krzysztof Nikiel <knik0@users.noreply.github.com>
2014-10-08 14:11:13 +0200

    replaced static config on PB3/4 with momentary PB4 switch and config saved to eeprom

--- main.c
+++ main.c
@@ -27,6 +27,8 @@ static volatile uint8_t g_tcnt;
 #define CBI(port,val) asm volatile ("cbi %0,%1"::"I" (&(port)-32), "I"(val))
 #define SBI(port,val) asm volatile ("sbi %0,%1"::"I" (&(port)-32), "I"(val))
 
+#define PORTBMASK (M(PB_LED1)|M(PB_LED2))
+
 // PORTB:
 enum
 { PB_LED1 = 0, PB_LED2 = 1 };
@@ -45,6 +47,57 @@ static inline uint16_t rand()
   return g_lfsr;
 }
 
+
+static uint8_t eeget(uint16_t addr)
+{
+  EEARL = addr;
+  SBI(EECR, EERE);
+  return EEDR;
+}
+
+static void eeput(uint16_t addr, uint8_t data)
+{
+  uint8_t savesreg = SREG;
+
+  asm volatile ("cli");
+
+  EECR = 0;
+  EEARL = addr;
+  EEDR = data;
+
+  SBI(EECR, EEMPE);
+  SBI(EECR, EEPE);
+  while (EECR & (1 << EEPE));
+
+  SREG = savesreg;
+}
+
+enum
+{ CFG_MODE = 0x80, CFG_DMAX = 2 };
+static uint8_t g_cfg;
+static void cfg(uint8_t update)
+{
+  enum
+  { EE_CFG = 2 };
+  g_cfg = eeget(EE_CFG);
+  if (g_cfg == 0xff)
+    g_cfg = 0;
+
+  if ((g_cfg & 3) > CFG_DMAX)
+    g_cfg &= ~3;
+
+  if (update)
+  {
+    if ((++g_cfg & 3) > CFG_DMAX)
+    {
+      g_cfg ^= CFG_MODE;
+      g_cfg &= ~3;
+    }
+
+    eeput(EE_CFG, g_cfg);
+  }
+}
+
 static void candle(void)
 {
   // OCA&OCB PWM enabled; fast PWM mode 3
@@ -59,6 +112,57 @@ static void candle(void)
   {
     OCR0B = v;
   }
+
+  void sleep(uint8_t cnt)
+  {
+    g_tcnt = cnt;
+    while (g_tcnt)
+      asm volatile ("sleep");
+  }
+  void showcfg(void)
+  {
+    uint8_t cnt;
+
+    DDRB = 0;
+    out1(0xf0);
+    out2(0xf0);
+
+    for (cnt = 0; cnt < 4; cnt++)
+      sleep(255);
+
+    cnt = 2;
+    if (g_cfg & CFG_MODE)
+      cnt = 4;
+
+    while (cnt)
+    {
+      cnt--;
+
+      sleep(255);
+      sleep(255);
+
+      switch (g_cfg & 3)
+      {
+      case 0:
+	DDRB = M(PB_LED1);
+	break;
+      case 1:
+	DDRB = M(PB_LED2);
+	break;
+      case 2:
+	DDRB = M(PB_LED1) | M(PB_LED2);
+	break;
+      }
+
+      sleep(20);
+      DDRB = 0;
+    }
+
+    DDRB = PORTBMASK;
+  }
+
+  cfg(FALSE);
+  //showcfg();
 
   out1(0);
   out2(0);
@@ -85,7 +189,7 @@ static void candle(void)
     pow += powad / 4;
     pow -= (pow - POWMID) / 8;
 
-    if (PINB & M(PINB3))
+    if (g_cfg & CFG_MODE)
     {
       // PB3=1 -> mode 1
 
@@ -124,16 +228,29 @@ static void candle(void)
 
     pow2 = ((uint16_t) pow * (uint16_t) pow) >> 8;
 
+    if (!(PINB & M(PINB4)))
+    {
+      cfg(TRUE);
+      showcfg();
+      while (!(PINB & M(PINB4)));
+    }
+
     // wait for sync
     while (g_tcnt)
       asm volatile ("sleep");
     // set delay
-    if (PINB & M(PINB4))
-      // PB4=1 -> slow
-      g_tcnt = 20;
-    else
-      // PB4=0 -> fast
+    switch (g_cfg & 3)
+    {
+    case 0:
       g_tcnt = 16;
+      break;
+    case 1:
+      g_tcnt = 18;
+      break;
+    default:
+      g_tcnt = 20;
+      break;
+    }
 
     out1(((uint16_t) pos * pow2) >> 8);
     out2(((uint16_t) (0xff - pos) * pow2) >> 8);
@@ -145,9 +262,8 @@ int main(void) __attribute__ ((noreturn));
 int main(void)
 {
   // init:
-#define PORTBMASK (M(PB_LED1)|M(PB_LED2))
   DDRB = PORTBMASK;
-  PORTB = ((uint8_t) ~ PORTBMASK) & ~(M(PINB3) | M(PINB4));
+  PORTB = ((uint8_t) ~ PORTBMASK);
 
   // system clock prescaler:
   CLKPR = M(CLKPCE);
